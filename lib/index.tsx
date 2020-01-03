79ac2e6ca68915c0868992672cce11b2fb031012
BrendanZhang <ex.arc.zhang@gmail.com>
2020-01-04 04:51:32 +0800

    add npm token

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,14 +1,1 @@
-import ReactDOM from "react-dom"
-import React from "react"
-import Icon from "./icon/icon"
-
-// const fn: React.MouseEventHandler<Element> = (e) => {
-// 	console.log(e.target)
-// }
-
-ReactDOM.render(
-	<div>
-		<Icon name="qq" onMouseEnter={() => console.log("enter")} onMouseLeave={() => console.log("leave")} />
-	</div>,
-	document.querySelector("#root"),
-)
+export { default as Icon } from "./icon/icon"
