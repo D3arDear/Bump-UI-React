d103c05b57bd69c5a87a2280c00668240be212de
BrendanZhang <ex.arc.zhang@gmail.com>
2020-01-06 02:42:06 +0800

    hasAside rewrite

--- lib/layout/layout.tsx
+++ lib/layout/layout.tsx
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { ReactElement } from "react";
 import { scopeClassMaker } from "../classes";
 import "./layout.scss";
+import Aside from "./aside";
 const sc = scopeClassMaker("bui-layout");
 
-interface Props extends React.HTMLAttributes<HTMLElement> {}
+interface Props extends React.HTMLAttributes<HTMLElement> {
+	children: ReactElement | Array<ReactElement>;
+}
 
 const Layout: React.FunctionComponent<Props> = (props) => {
 	const { className, ...rest } = props;
+	const childrenAsArray = props.children as Array<ReactElement>;
+	const hasAside =
+		childrenAsArray.length && childrenAsArray.reduce((result, node) => result || node.type === Aside, false);
 
 	return (
-		<div className={sc("", { extra: className })} {...rest}>
+		<div className={sc("", { extra: [className, hasAside && "hasAside"].join(" ") })} {...rest}>
 			{props.children}
 		</div>
 	);
