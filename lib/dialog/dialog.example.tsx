9857376549601dc59c051352f0507f785e4bf4ca
BrendanZhang <ex.arc.zhang@gmail.com>
2020-01-05 08:10:01 +0800

    dialog onClickClose and closeOnClickMask done

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -3,12 +3,38 @@ import Dialog from "./dialog"
 
 const DialogExample = () => {
 	const [x, setX] = useState(false)
+	const [y, setY] = useState(false)
 	return (
 		<div>
-			<button onClick={() => setX(!x)}>click</button>
-			<Dialog visible={x}>
-				<strong>hi</strong>
-			</Dialog>
+			<div>
+				<h2>Dialog close while click mask</h2>
+				<p>closeOnClickMask = true</p>
+				<button onClick={() => setX(!x)}>click</button>
+				<Dialog
+					visible={x}
+					buttons={[<button>1</button>, <button>2</button>]}
+					onClose={() => {
+						setX(false)
+					}}
+					closeOnClickMask={true}
+				>
+					<strong>hi</strong>
+				</Dialog>
+			</div>
+			<div>
+				<h2>Dialog wont close while click mask</h2>
+				<p>closeOnClickMask = false</p>
+				<button onClick={() => setY(!y)}>click</button>
+				<Dialog
+					visible={y}
+					buttons={[<button>1</button>, <button>2</button>]}
+					onClose={() => {
+						setY(false)
+					}}
+				>
+					<strong>hi</strong>
+				</Dialog>
+			</div>
 		</div>
 	)
 }
