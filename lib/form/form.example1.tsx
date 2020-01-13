23d2a7234632d753970820c1f4190c475efade53
BrendanZhang <ex.arc.zhang@gmail.com>
2020-01-13 14:02:47 +0800

    基本实现 form 功能

--- lib/form/form.example1.tsx
+++ lib/form/form.example1.tsx
@@ -1,26 +1,33 @@
 import React, { useState, Fragment } from "react";
-import Form from "./form";
+import Form, { FormValue } from "./form";
 
 export default function FormExample1() {
-  const [formData] = useState({
-    username: "",
-    password: "",
+  const [formData, setFromData] = useState<FormValue>({
+    username: "brenz",
+    password: "Skyrim",
   });
   const [fields] = useState([
     { name: "username", label: "用户名", input: { type: "text" } },
     { name: "password", label: "密码", input: { type: "password" } },
   ]);
-
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    console.log(formData);
+  };
   return (
-    <Form
-      value={formData}
-      fields={fields}
-      buttons={
-        <Fragment>
-          <button type="submit">提交</button>
-          <button>返回</button>
-        </Fragment>
-      }
-    />
+    <Fragment>
+      {JSON.stringify(formData)}
+      <Form
+        value={formData}
+        fields={fields}
+        buttons={
+          <Fragment>
+            <button type="submit">提交</button>
+            <button>返回</button>
+          </Fragment>
+        }
+        onSubmit={onSubmit}
+        onChange={(newValue) => setFromData(newValue)}
+      />
+    </Fragment>
   );
 }
