0802da6264a76fe74e92cb6b5e1dadfc16442b22
BrendanZhang <ex.arc.zhang@gmail.com>
2020-01-15 08:30:34 +0800

    input css done

--- lib/input/input.tsx
+++ lib/input/input.tsx
@@ -8,9 +8,10 @@ interface Props extends InputHTMLAttributes<HTMLInputElement> {
 
 const Input: React.FunctionComponent<Props> = (props) => {
   const { className, label, value, ...rest } = props;
+  const onFocus = () => (value ? "focus" : "");
   return (
-    <div className={classes("bui-input-wrapper", className)}>
-      <div className={classes("bui-input-label")}>
+    <div className={classes("bui-input-wrapper root-input", className)}>
+      <div className={classes(`bui-input-label ${onFocus()}`)}>
         <span>{label}</span>
       </div>
       <input className={classes("bui-input", className)} value={value} {...rest} />
