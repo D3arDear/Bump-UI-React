c5094373fc6a996a58dcbc075cc10e54ae6738db
BrendanZhang <ex.arc.zhang@gmail.com>
2020-02-22 20:53:20 +0800

    init scrollbar and track

--- lib/scroll/scrollArea.tsx
+++ lib/scroll/scrollArea.tsx
@@ -1,4 +1,4 @@
-import React, { HTMLAttributes } from "react";
+import React, { HTMLAttributes, UIEventHandler, useState, useEffect, useRef } from "react";
 import "./scroll.scss";
 import scrollbarWidth from "./scrollbar-width";
 
@@ -6,10 +6,25 @@ interface Props extends HTMLAttributes<HTMLDivElement> {}
 
 const ScrollArea: React.FunctionComponent<Props> = (props) => {
   const { children, ...rest } = props;
+  const [barHeight, setBarHeight] = useState(0);
+  const onScroll: UIEventHandler = (e) => {
+    // const scrollHeight = e.currentTarget.scrollHeight;
+    // const viewHeight = e.currentTarget.getBoundingClientRect().height;
+    // setBarHeight((viewHeight * viewHeight) / scrollHeight);
+  };
+  const containerRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const scrollHeight = containerRef.current!.scrollHeight;
+    const viewHeight = containerRef.current!.getBoundingClientRect().height;
+    setBarHeight((viewHeight * viewHeight) / scrollHeight);
+  }, []);
   return (
     <div className="bui-scroll" {...rest}>
-      <div className="bui-scroll-inner" style={{ right: -scrollbarWidth() }}>
+      <div className="bui-scroll-inner" style={{ right: -scrollbarWidth() }} ref={containerRef} onScroll={onScroll}>
         {children}
+      </div>
+      <div className="bui-scroll-track">
+        <div className="bui-scroll-bar" style={{ height: barHeight }}></div>
       </div>
     </div>
   );
