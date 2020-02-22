09c7765b0ed513d6e1f2b8eeaada4c030b8c0f6b
BrendanZhang <ex.arc.zhang@gmail.com>
2020-02-22 21:02:30 +0800

    scrollBar scrollable

--- lib/scroll/scrollArea.tsx
+++ lib/scroll/scrollArea.tsx
@@ -7,10 +7,13 @@ interface Props extends HTMLAttributes<HTMLDivElement> {}
 const ScrollArea: React.FunctionComponent<Props> = (props) => {
   const { children, ...rest } = props;
   const [barHeight, setBarHeight] = useState(0);
+  const [barTop, setBarTop] = useState(0);
   const onScroll: UIEventHandler = (e) => {
-    // const scrollHeight = e.currentTarget.scrollHeight;
-    // const viewHeight = e.currentTarget.getBoundingClientRect().height;
-    // setBarHeight((viewHeight * viewHeight) / scrollHeight);
+    const { current } = containerRef;
+    const scrollHeight = current!.scrollHeight;
+    const viewHeight = current!.getBoundingClientRect().height;
+    const scrollTop = current!.scrollTop;
+    setBarTop((scrollTop * viewHeight) / scrollHeight);
   };
   const containerRef = useRef<HTMLDivElement>(null);
   useEffect(() => {
@@ -24,7 +27,7 @@ const ScrollArea: React.FunctionComponent<Props> = (props) => {
         {children}
       </div>
       <div className="bui-scroll-track">
-        <div className="bui-scroll-bar" style={{ height: barHeight }}></div>
+        <div className="bui-scroll-bar" style={{ height: barHeight, transform: `translateY(${barTop}px)` }}></div>
       </div>
     </div>
   );
