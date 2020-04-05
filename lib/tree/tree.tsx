48de5e1d1813aefd773cae753c4c98c2bb94b489
BrendanZhang <ex.arc.zhang@gmail.com>
2020-04-05 15:41:25 +0800

    style match level

--- lib/tree/tree.tsx
+++ lib/tree/tree.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./tree.scss";
+import { scopeClassMaker } from "../helpers/classes";
 
 interface SourceDataItem {
   text: string;
@@ -9,12 +10,19 @@ interface SourceDataItem {
 interface Props {
   sourceData: SourceDataItem[];
 }
-const x = (item: SourceDataItem) => {
+const scopedClass = scopeClassMaker("bui-tree");
+const sc = scopedClass;
+const renderItem = (item: SourceDataItem, level = 1) => {
+  const classes = {
+    ["level-" + level]: true,
+    item: true,
+  };
+
   return (
-    <div key={item.value}>
+    <div key={item.value} className={sc(classes)}>
       {item.text}
       {item.children?.map((subItem) => {
-        return x(subItem);
+        return renderItem(subItem, level + 1);
       })}
     </div>
   );
@@ -24,7 +32,7 @@ const Tree: React.FC<Props> = (props) => {
   return (
     <div>
       {props.sourceData?.map((item) => {
-        return x(item);
+        return renderItem(item);
       })}
     </div>
   );
